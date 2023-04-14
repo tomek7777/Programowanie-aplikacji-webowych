e20afd80649772f1a17269c8442701d80cbaa63b
tomek7777 <89088480+tomek7777@users.noreply.github.com>
2023-04-14 13:05:42 +0200

    lab5

---
+++ lab5/reactiveForms/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss']
+})
+export class ReactiveFormsComponent {
+  name = new FormControl('');
+}
